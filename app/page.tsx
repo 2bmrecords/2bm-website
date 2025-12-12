52e6ca680c4c7b96112d8f79a5d72a0eb913e8aa
Sehajpreet Singh <sehajpreetsingh@MacBook-Pro-2.local>
2025-12-12 13:14:42 -0500

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -46,10 +46,7 @@ export default function Home() {
                 width={500}
                 height={100}
                 className="max-w-[400px] h-auto object-contain invert"
-                style={{
-                  imageRendering: 'pixelated',
-                  filter: 'contrast(125%) drop-shadow(3px 3px 0px #000)'
-                }}
+                style={{ filter: 'drop-shadow(0 0 2px rgba(0,0,0,1)) drop-shadow(0 0 2px rgba(0,0,0,1)) drop-shadow(0 0 3px rgba(0,0,0,0.8))' }}
                 priority
               />
             </FadeIn>
